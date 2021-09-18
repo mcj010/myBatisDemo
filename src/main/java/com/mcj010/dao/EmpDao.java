ff029603461fb14f05d932e0383ffe4f5cb2e167
mcj010 <329392494@qq.com>
2021-09-18 21:37:42 +0800

    add log4j

--- src/main/java/com/mcj010/dao/EmpDao.java
+++ src/main/java/com/mcj010/dao/EmpDao.java
@@ -1,11 +1,19 @@
 package com.mcj010.dao;
 
 import com.mcj010.bean.Emp;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 public interface EmpDao {
 
-    public void save(Emp e);
+    public Integer save(Emp e);
+
     public Integer update(Emp emp);
+
     public Integer delete(Integer empNo);
+
     public Emp selectEmpByEmpNo(Integer empNo);
+
+    @Select("select * from emp where ename = #{ename} and sal = #{sal}")
+    public Emp selectEmpByEnameAndSal(@Param("ename") String ename, @Param("sal") Double sal);
 }
--- src/test/java/Test01.java
+++ src/test/java/Test01.java
@@ -1,24 +1,87 @@
 import com.mcj010.bean.Emp;
 import com.mcj010.dao.EmpDao;
+import junit.framework.TestCase;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
 import java.io.IOException;
 import java.io.InputStream;
 
+@FixMethodOrder(MethodSorters.DEFAULT)
 public class Test01 {
+    SqlSessionFactory sqlSessionFactory = null;
+
+    @Before
+    public void init() {
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = null;
+        try {
+            inputStream = Resources.getResourceAsStream(resource);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+    }
 
     @Test
-    public void test01() throws IOException {
-        String resource = "mybatis-config.xml";
-        InputStream inputStream = Resources.getResourceAsStream(resource);
-        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+    public void testASelect() {
         SqlSession sqlsesson = sqlSessionFactory.openSession();
         EmpDao dao = sqlsesson.getMapper(EmpDao.class);
         Emp emp = dao.selectEmpByEmpNo(1);
+        System.out.println(emp);
+        sqlsesson.close();
+    }
+
+    @Test
+    public void testBSave() {
+        SqlSession ss = sqlSessionFactory.openSession();
+        EmpDao dao = ss.getMapper(EmpDao.class);
+        Emp emp = new Emp();
+        emp.setEmpno(3);
+        emp.setEname("bb");
+        emp.setCommon(12.2);
+        Integer i = dao.save(emp);
+        System.out.println(i);
+        ss.commit();
+        ss.close();
+    }
+
+    @Test
+    public void testCUpdate() {
+        SqlSession ss = sqlSessionFactory.openSession(true);
+        EmpDao dao = ss.getMapper(EmpDao.class);
+        Emp emp = new Emp();
+        emp.setEmpno(3);
+        emp.setSal(1000.1);
+        Integer i = dao.update(emp);
+        System.out.println(i);
+        ss.close();
+    }
+
+    @Test
+    public void testDDelete() {
+        SqlSession ss = sqlSessionFactory.openSession();
+        EmpDao dao = ss.getMapper(EmpDao.class);
+        Integer i = dao.delete(3);
+        System.out.println(i);
+        ss.commit();
+        ss.close();
+    }
+
+    @Test
+    public void testESelect() {
+        SqlSession sqlsesson = sqlSessionFactory.openSession();
+        EmpDao dao = sqlsesson.getMapper(EmpDao.class);
+        Emp emp = new Emp();
+//        emp.setEname("jack");
+//        emp.setSal(1000.0);
+        emp = dao.selectEmpByEnameAndSal("jack",1000.0);
         System.out.println(emp);
         sqlsesson.close();
     }
