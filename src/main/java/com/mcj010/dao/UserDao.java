c3e1b33f9b37f01dfb0088cad39d0605d5c98376
mcj010 <329392494@qq.com>
2021-09-19 22:21:44 +0800

    加了注释

--- src/main/java/com/mcj010/dao/UserDao.java
+++ src/main/java/com/mcj010/dao/UserDao.java
@@ -1,9 +1,11 @@
 package com.mcj010.dao;
 
 import com.mcj010.bean.User;
+import org.apache.ibatis.annotations.Select;
 
 public interface UserDao {
 
+    @Select("select * from user where id = #{id}")
     public User selectUserById(Integer id);
 
 }
